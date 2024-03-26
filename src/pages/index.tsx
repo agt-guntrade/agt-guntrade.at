d39876fa9bf48103fbe60520935af8cf56f96d35
Nico Schett <nicoschett@icloud.com>
2024-03-26 16:37:28 +0100

    feat: adjust opacity and add former business name
    Adjusted the opacity value and added the former business name to the index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,15 @@ const IndexPage: React.FC<PageProps> = () => {
             alt="Left Image"
             className="h-full max-w-full object-cover"
           />
-          <div className="absolute inset-0 rounded-md bg-gray-700 opacity-60"></div>
+          <div className="absolute inset-0 rounded-md bg-gray-700 opacity-70"></div>
           <div className="absolute inset-0 flex flex-col items-center justify-center p-4">
             <h2 className="mb-2 scale-100 transform text-3xl font-bold text-white transition-transform">
               Filiale <span className="text-red-500">Ferlach</span>
             </h2>
+            <h2 className="mb-2 scale-100 transform text-2xl font-bold text-white transition-transform">
+              (Ehemalig Waffenhandel Türk)
+            </h2>
+
             <p className="mb-4 text-white">Reßnig 20, 9170 Ferlach</p>
             <Button className="bg-red-500" size="lg">
               Zur Filiale
